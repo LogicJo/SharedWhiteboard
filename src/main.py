38234ca5d1c8cfa9d2a42a0892ee6c2867ea7ea4
UserFrosch <lukasmoebs@gmx.de>
2026-04-26 17:17:17 +0200

    join room and connect

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, url_for, render_template, session, redirect, request
-from flask_socketio import SocketIO
+from flask import Flask, url_for, render_template, session, redirect, request, flash, get_flashed_messages
+from flask_socketio import SocketIO, join_room
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -10,22 +10,32 @@ rooms = []
 
 @app.route("/")
 def index():
-    if not session.get('name'):
+    if not(session.get('name') and session.get('room')):
         return redirect(url_for('login'))
     return render_template("index.html")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    error = None
     if request.method == 'POST':
         if form.validate_on_submit():
             name = form.name.data
             room = form.room.data
             if form.join.data:
-                pass
+                if not room in rooms:
+                    error = "Room doesn't exist"
             if form.create.data:
-                pass
-            session['name'] = name
-            session['room'] = room
+                if room in rooms:
+                    error = "Room already exists"
+            if not error:
+                session['name'] = name
+                session['room'] = room
+                return redirect(url_for('index'))
+    return render_template("login.html", form=form, error=error)
 
-    return render_template("login.html", form=form)
+@socketio.on('connect')
+def connect(data):
+    room = session['room']
+    join_room(room)
+    print(f"connected to room: {room}")
